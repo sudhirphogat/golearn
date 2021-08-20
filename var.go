2f4ab30abb5b936261e63e10492fb414d5bebb6d
sudhirphogat <sudhir.phogat87@gmail.com>
2021-08-20 19:44:38 +0530

    variables explained

---
+++ var.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var y = 43 // scope of this variable is entire program (package level)
+
+//** limit the scope of the variable so declare insid the function to avoid confussion
+var z int
+
+// this will assign 0 value to Z if we do not assign any values
+
+func main() {
+	x := 42 // := shout declaration needs to be printed inside the curly braces
+	fmt.Println(x)
+
+	//var y = 43 //var can be declared ouside the function also
+	fmt.Println(y)
+
+	food()
+	//fmt.Println(z) // No error will be thrown in case I do not use the var z
+}
+
+//example of var scope
+func food() {
+	fmt.Println("again:", y)
+}
