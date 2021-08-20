2dad7c855aa8ef810fa7b254f2dc984e3dc97830
sudhirphogat <sudhir.phogat87@gmail.com>
2021-08-20 18:49:18 +0530

    functions and declaration

--- hello.go
+++ hello.go
@@ -11,8 +11,19 @@ func main() {
 			fmt.Println(i)
 		}
 	}
+	check()
 }
 
 func foo() {
 	fmt.Print("I am in foo")
+}
+
+func check() {
+	n, _ := fmt.Println(37, true)
+	fmt.Println(n)
+	//fmt.Println(err)
+	//n returns number of bytes and err returns if there are any errors
+	//can pass boolen and integer without ""
+
+	//_ is given instead of err in case we dont want to declare a variable because there should be no unused variables
 }
